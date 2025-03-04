78646ef1a14bbf2074511ea6d94d60221c074e8e
Sebastian Wypiór <wypior.sebastian.dev@gmail.com>
2025-03-04 06:26:05 +0100

    ss

---
+++ CosmosDbTrigger/src/UrlShortener.CosmosDbTriggerFunction/ShortUrlPropagation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace UrlShortener.CosmosDbTriggerFunction;
+
+public class ShortUrlPropagation
+{
+    private readonly ILogger<ShortUrlPropagation> _logger;
+
+    public ShortUrlPropagation(ILogger<ShortUrlPropagation> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("ShortUrlPropagation")]
+    public void Run([CosmosDBTrigger(
+            databaseName: "urls",
+            containerName: "items",
+            Connection = "",
+            LeaseContainerName = "leases",
+            CreateLeaseContainerIfNotExists = true)]
+        IReadOnlyList<MyDocument> input)
+    {
+        if (input != null && input.Count > 0)
+        {
+            _logger.LogInformation("Documents modified: " + input.Count);
+            _logger.LogInformation("First document Id: " + input[0].Id);
+        }
+
+        
+    }
+}
+
+public class MyDocument
+{
+    public string Id { get; set; }
+
+    public string Text { get; set; }
+
+    public int Number { get; set; }
+
+    public bool Boolean { get; set; }
+}
